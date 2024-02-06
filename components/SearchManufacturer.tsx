be172dcb461ef8ba7919028d66044e6cd7382098
Edward Kim <itgma1231@gmail.com>
2024-02-05 19:35:43 -0500

    Combobox/Transition headlessui components setup in SearchManufacturer.tsx

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,50 @@
-import { SearchManufacturerProps } from '@/types'
+'use client';
+import { useState, Fragment } from 'react';
+import Image from 'next/image';
+import { Combobox, Transition } from '@headlessui/react';
 
-const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+import { manufacturers } from '@/constants';
+import { SearchManufacturerProps } from '@/types';
+
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState('');
+
   return (
-    <div>SearchManufacturer</div>
-  )
-}
+    <div className='search-manufacturer'>
+      <Combobox>
+        <div className='relative w-full'>
+          <Combobox.Button className='absolute top-[14px]'>
+            <Image
+              src='/car-logo.svg'
+              alt='Car Logo'
+              width={20}
+              height={20}
+              className='ml-4'
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className='search-manufacturer__input'
+            placeholder='Volkswagen'
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+      </Combobox>
+      <Transition
+        as={Fragment}
+        // show={}
+        leave='transition ease-in duration-100'
+        leaveFrom='opacity-100'
+        leaveTo='opacity-0'
+        afterLeave={() => setQuery('')}
+      >
+        <Combobox.Options></Combobox.Options>
+      </Transition>
+    </div>
+  );
+};
 
-export default SearchManufacturer
+export default SearchManufacturer;
