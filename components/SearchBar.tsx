a1826cf71b24e93939d599ef40505947f677a863
Edward Kim <itgma1231@gmail.com>
2024-02-05 19:02:24 -0500

    SearchManufacturer.tsx created/imported into SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+'use client';
+import { useState } from 'react';
+
+import SearchManufacturer from './SearchManufacturer';
 
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
-}
+  const [manufacturer, setManufacturer] = useState('');
 
-export default SearchBar
+  const handleSearch = () => {};
+
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar_item'>
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
