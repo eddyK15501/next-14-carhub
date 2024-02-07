043f543d42df0877bbe14fc6ab10ba944df49654
Edward Kim <itgma1231@gmail.com>
2024-02-06 19:19:43 -0500

    update state in SearchBar.tsx from Combobox

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,6 +5,7 @@ import SearchManufacturer from './SearchManufacturer';
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
+  // console.log(manufacturer);
 
   const handleSearch = () => {};
 
