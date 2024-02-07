520505983f0ca67b711f4c8854fbeefd04a72f57
Edward Kim <itgma1231@gmail.com>
2024-02-06 19:14:26 -0500

    Combobox in SearchManufacturer.tsx complete

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -50,19 +50,35 @@ const SearchManufacturer = ({
           >
             <Combobox.Options>
               {filteredManufacturers.map((item) => (
-                  <Combobox.Option
-                    key={item}
-                    value={item}
-                    className={({ active }) =>
-                      `relative search-manufacturer__option ${
-                        active ? 'bg-primary-blue text-white' : 'text-gray-900'
-                      }`
-                    }
-                  >
-                    {item}
-                  </Combobox.Option>
-                ))
-              }
+                <Combobox.Option
+                  key={item}
+                  value={item}
+                  className={({ active }) =>
+                    `relative search-manufacturer__option ${
+                      active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                    }`
+                  }
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {item}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? 'text-white' : 'text-teal-600'
+                          }`}
+                        ></span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))}
             </Combobox.Options>
           </Transition>
         </div>
