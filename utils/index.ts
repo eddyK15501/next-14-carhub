b487f192aa75758e0f2f49e042dfcd878295c72a
Edward Kim <itgma1231@gmail.com>
2024-02-07 20:12:37 -0500

    Free tier RapidAPI-Key stored in .env; npm install dotenv

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const fetchCars = async () => {
   try {
     const headers = {
-      'X-RapidAPI-Key': '49dedabdb9mshbd7ad33c5a56ca1p183279jsneb5228f24a81',
+      'X-RapidAPI-Key': `${process.env.API_KEY}`,
       'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
     };
 
