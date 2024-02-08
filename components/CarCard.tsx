30c0fd08167b9d406e3e8084a70434de89d58ba6
Edward Kim <itgma1231@gmail.com>
2024-02-07 19:16:46 -0500

    CarDetails.tsx prop types defined in interface

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -72,7 +72,7 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
-      <CarDetails />
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
     </div>
   );
 };
